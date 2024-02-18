b0695d4ab251eef7bc29e68aae6a6bd126b1564c
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-02-18 16:56:56 +0530

    updated

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Welcome from './components/Welcome'
+import Magical from './components/Magical'
 const App = () => {
   return (
     <>
     <NavBar/> 
     <Welcome/>
+    <Magical/>
     </>
   )
 }
---
+++ project/src/components/Magical.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import magicImg1 from '../assets/wedding-scroll-cropped.jpg'
+import magicImg2 from '../assets/2.2-Social-Lorely-Meza.jpg'
+import magicImg3 from '../assets/2.3-Corporate-Joel-Maus.jpg'
+import magicImg4 from '../assets/2.4-Venues-Jenny-Quicksall.jpg'
+const Magical = () => {
+  return (
+	<div className="magical-components">
+		<div className="magical-left">
+
+		</div>
+		<div className="magical-right">
+
+		</div>
+	</div>
+  )
+}
+
+export default Magical
