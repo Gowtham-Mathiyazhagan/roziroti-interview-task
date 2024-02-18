c43ec663b4a040de41a2f97930bf467a3b32ad41
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-02-18 09:27:10 +0530

    updated

--- project/src/components/Welcome.jsx
+++ project/src/components/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
             <h6>WELCOME TO 24 CARROTS</h6>
 			<h1>Remarkable <br /> Catering & <br /> Events</h1>
 			<p>24 Carrots is the premier catering and events company of choice in Southern California. We create remarkable experiences by offering the finest quality foods and providing unsurpassed personalized service, driven by our passion for life’s special occasions.</p>
-		    <div className="button">GET IN TOUCH</div>
+		    <div className="button">GET IN TOUCH <i class="bi bi-arrow-right"></i></div>
 		</div>
 		<div className="welcome-right">
           <img src={img1} alt="img"/>
