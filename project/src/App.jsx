c0823f54b9dfb13b28393cd8d762df846929707b
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-02-23 10:28:11 +0530

    updated

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -6,6 +6,7 @@ import Venues from './components/Venues'
 import Carousel from './components/Carousel'
 import Business from './components/Business'
 import GreetFood from './components/GreetFood'
+import Services from './components/Services'
 const App = () => {
   return (
     <>
@@ -16,6 +17,7 @@ const App = () => {
     <Carousel/>
     <Business/>
     <GreetFood/>
+    <Services/>
     </>
   )
 }
--- project/src/components/Services.jsx
+++ project/src/components/Services.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import img from "../assets/Home_Services_GIF_R3.gif";
 const Services = () => {
   return (
-  )
-}
+    <div className="service-component">
+      <div className="service-left">
+        <div className="first-child">
+          <h5>Gourmet Catering</h5>
+          <p>
+            Our professionally-trained culinary team is passionate and proud of
+            our diverse and thoughtful menu, always cooking up something that
+            aligns with your great taste.
+          </p>
+          <div className="button">
+            LEARN MORE <i className="bi bi-arrow-right"></i>
+          </div>
+        </div>
+        <div className="second-child">
+          <h5>Bar Service</h5>
+          <p>
+            Our sophisticated flavor expertise enables us to create inventive
+            concoctions that generate buzz in more ways than one!
+          </p>
+          <div className="button">
+            LEARN MORE <i className="bi bi-arrow-right"></i>
+          </div>
+        </div>
+      </div>
+      <div className="service-mid">
+        <img src={img} alt="" />
+      </div>
+      <div className="service-right">
+        <div className="first-child">
+          <h5>Staffing</h5>
+          <p>
+            The hand-selected team at 24 Carrots are just as important to our
+            reputation as our gourmet food. Our staff is certainly the best at
+            what they do, and you’ll work with professionals who genuinely care
+            about the success of your event!{" "}
+          </p>
+          <div className="button">
+            LEARN MORE <i className="bi bi-arrow-right"></i>
+          </div>
+        </div>
+        <div className="second-child">
+          <h5>Event Production</h5>
+          <p>
+            If mind-blowing spectacles and immersive guest experiences are what
+            you’re after, the specialists at 24 Carrots can pull it off on an
+            epic scale!
+          </p>
+          <div className="button">
+            LEARN MORE <i className="bi bi-arrow-right"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
